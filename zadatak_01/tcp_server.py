8e9a6261850f18c7f9e61f68286d7c3643ceb699
Pavle Ciric <pavle@ciric.me>
2022-08-05 16:36:01 +0200

    [edit] zadatak_01

--- zadatak_01/tcp_server.py
+++ zadatak_01/tcp_server.py
@@ -6,17 +6,18 @@ if len(sys.argv) < 2:
 
 LISTEN_ADDR = sys.argv[1]
 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+s.bind((LISTEN_ADDR, 1234))
+
 while True:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-    s.bind((LISTEN_ADDR, 1234))
     s.listen(5)
-
 
     clientsocket, address = s.accept()
     clientsocket.send(bytes("Dobrodosao na server!", "utf-8"))
     print(f"Client connected: {address}")
 
-    s.close()
+    clientsocket.close()
 
