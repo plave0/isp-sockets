b8631ea1c47f72a10c19d91262e467b75606461a
Pavle Ciric <pavle@ciric.me>
2022-08-07 19:28:17 +0200

    [edit] zadatak_02

--- zadatak_02/tcp_client.py
+++ zadatak_02/tcp_client.py
@@ -1,16 +1,17 @@
 import socket
 import sys
 
-if len(sys.argv) < 2:
+if len(sys.argv) < 3:
     exit(1)
 
 SERVER_ADDR = sys.argv[1]
+PORT = int(sys.argv[2])
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((SERVER_ADDR, 1234))
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.connect((SERVER_ADDR, 1234))
 
-    msg = input()
-    s.sendall(bytes(msg, "utf-8"))
-    server_msg = s.recv(1024)
+msg = input()
+server_socket.sendall(bytes(msg, "utf-8"))
 
+server_msg = server_socket.recv(1024)
 print(f"Server message: {server_msg.decode('utf-8')}")
--- zadatak_02/tcp_server.py
+++ zadatak_02/tcp_server.py
@@ -1,29 +1,40 @@
 import socket
 import sys
 
-if len(sys.argv) < 2:
+if len(sys.argv) < 3:
     exit(1)
 
 LISTEN_ADDR = sys.argv[1]
+LISTEN_PORT = int(sys.argv[2])
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-s.bind((LISTEN_ADDR, 1234))
+listen_socket.bind((LISTEN_ADDR, LISTEN_PORT))
+listen_socket.listen()
 
-while True:
+server_running = True
+while server_running:
 
-    s.listen(5)
+    try:
 
-    clientsocket, address = s.accept()
-    with clientsocket:
+        client_socket, address = listen_socket.accept()
 
-        print(f"Client connected: {address}")
+    except KeyboardInterrupt:
 
-        msg = clientsocket.recv(1024)
-        print(f"Client message: {msg.decode('utf-8')}")
+        print("Exiting server...")
 
-        clientsocket.sendall(bytes("Message received", "utf-8"))
+        listen_socket.close()
+        server_running = False
+        continue
 
-    clientsocket.close()
+
+    print(f"Client connected: {address}")
+
+    msg = client_socket.recv(1024)
+    print(f"Message from {address}: {msg.decode('utf-8')}")
+
+    client_socket.sendall(bytes("Message received", "utf-8"))
+
+    client_socket.close()
 
